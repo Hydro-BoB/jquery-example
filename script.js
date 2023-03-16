9209f9ec7db978a7010742054d31bde8fb28cbed
Hydro-BoB <54343070+Hydro-BoB@users.noreply.github.com>
2023-03-16 23:45:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+ 
+
+// $(document).ready(function(){
+//     $("h1").css("color", "red");
+// });
+
+
+// $(document).keydown(function(e){
+    // $("h1").text(e.key);
+    $("h1").text("bye");
+// });
+
+$("button").click(function(){
+    $("button").hide(1000);
+  });
